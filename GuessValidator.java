520abc70f44a53f9340e97016ea6d93e86e166e1
Ghouse <ghouseconnect@gmail.com>
2026-02-23 14:25:34 +0530

    Basic gameplay coded, can start, guess and end when guess is correct or exceeds attempts - UC2

---
+++ GuessValidator.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class GuessValidator{
+	public static String validateGuess(int guess, int target){
+		if(guess == target) return "CORRECT";
+		else if(guess < target) return "LOW";
+		return "HIGH";
+	}
+}
--- GuessingApp.java
+++ GuessingApp.java
@@ -1,11 +1,13 @@
 /*
 
-* Guessing App - UC1: Game initialization
-* Entry point and displays rules
-* No user input or gamplay logic implemented yet
+* Guessing App - UC2: User Guess Submission
+* Initializes Game
+* Accepts user guesses
+* Validate guesses
+* Stop when game ends
 
 * @author Developer
-* @version 1.0
+* @version 2.0
 
 */
 
@@ -15,7 +17,19 @@ import java.util.*;
 public class GuessingApp{
 	public static void main(String args[]){
 		System.out.println("Welcome to the Guessing App");
-		GameConfig gameConfig = new GameConfig();
-		gameConfig.showRules();
+		GameConfig config = new GameConfig();
+		config.showRules();
+		
+		Scanner scanner = new Scanner(System.in);
+		int attempts = 0;
+		
+		while(attempts < config.getMaxAttempts()){
+			System.out.print("Enter your guess: ");
+			int guess = scanner.nextInt();
+			attempts++;
+			String result = GuessValidator.validateGuess(guess, config.getTargetNumber());
+			System.out.println(result);
+			if(result.equals("CORRECT")) break;
+		}
 	}
 }
